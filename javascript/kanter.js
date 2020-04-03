8d93a5e843b77323c38181e816a8401b0b8d3ec0
Drag0m1r <545436@edu.rocmn.nl>
2020-04-03 13:03:25 +0200

    sidemenu and random stuff

---
+++ javascript/kanter.js
@@ -0,0 +1,8 @@
+
+function openMenu () {
+    document.getElementById("sidemenu").style.width = "300px";
+}
+function closeMenu () {
+    document.getElementById("sidemenu").style.width = "0px";
+}
+
