c52afb03425f933c0045be90beb0feae01feb7b3
Chris Guilliamse <39490830+CG364@users.noreply.github.com>
2020-04-05 23:28:43 +0200

    smaller images now scale correctly

--- javascript/memes.js
+++ javascript/memes.js
@@ -5,7 +5,7 @@ document.getElementById("nextMeme").addEventListener("click", nextMeme);
 document.getElementById("prevMeme").addEventListener("click", previousMeme);
 
 
-const memePaths = ["/images/memes/f80vlbb3wgq41.jpg", "images/memes/i5nqc7pcrlq41.jpg", "images/memes/k00yth3c1nq41.jpg", "images/memes/ted5j1pwnmq41.png", "images/memes/xyz0u0mvcmq41.jpg", "images/memes/yn9j2kyqinq41.jpg"];
+const memePaths = ["/images/memes/f80vlbb3wgq41.jpg", "images/memes/i5nqc7pcrlq41.jpg", "images/memes/k00yth3c1nq41.jpg", "images/memes/ted5j1pwnmq41.png", "images/memes/xyz0u0mvcmq41.jpg", "images/memes/yn9j2kyqinq41.jpg", "images/memes/s0gx14key0r41.jpg"];
 let currentmeme = 0;
 
 document.getElementById("memeImageHeader").innerHTML = "Meme 1 van " + memePaths.length;
