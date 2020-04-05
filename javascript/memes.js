a0577a356e18b0085cdbda03e313797c0c4e6e67
Chris Guilliamse <39490830+CG364@users.noreply.github.com>
2020-04-05 20:10:40 +0200

    fixed typo / correct event listeners addes

--- javascript/memes.js
+++ javascript/memes.js
@@ -1,3 +1,10 @@
+//Author: Chris Guilliamse
+
+//event listeners:
+document.getElementById("nextMeme").addEventListener("click", nextMeme);
+document.getElementById("prevMeme").addEventListener("click", previousMeme);
+
+
 const memePaths = ["/images/memes/f80vlbb3wgq41.jpg", "images/memes/i5nqc7pcrlq41.jpg", "images/memes/k00yth3c1nq41.jpg", "images/memes/ted5j1pwnmq41.png", "images/memes/xyz0u0mvcmq41.jpg", "images/memes/yn9j2kyqinq41.jpg"];
 let currentmeme = 0;
 
@@ -35,3 +42,4 @@ function previousMeme()
     let memeplus1 = currentmeme + 1;
     document.getElementById("memeImageHeader").innerHTML = "Meme " + memeplus1 + " van " + memePaths.length;
 }
+
